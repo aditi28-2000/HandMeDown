7e520efb657f736c974633fa83e28256d98e9308
aditi28-2000 <mondaladiti75@gmail.com>
2022-10-19 05:12:11 -0400

    changes in about page

--- about.php
+++ about.php
@@ -25,7 +25,7 @@
           </div>
         </div>
         <div style="margin-bottom:130px"class="col-lg-7 about wow fadeInRight">
-          <h3>About HandMeDown</h3>
+          <h3>About the HandMeDown Platform</h3>
           <p>In this platform users willing to sell their books can set their own price and state the book condition, if any customer intends to buy the book he/she may contact the seller through messaging provided on the website itself.As the books are sold by students no maintanence cost is charged like in stores and you can get books for cheap proffiting both the seller and buyer , thus making online purchasing cheaper than the offline one. We would provide general registration and login functionality for users. They can browse through the website , search for their required books and consequently add to cart if they do not wish to purchase immediately otherwise directly order/purchase them. Similarly users who intend to sell their books can upload the details of the books and post online for potential customers to view and purchase.</p>
           <p>Online Book Exchange is the practice of a swap of books between one person and another. Practiced among book groups, friends and colleagues at work, it provides an inexpensive way for people to exchange books, find out about new books and obtain a new book to read without having to pay. Because swaps occur between individuals, without central distribution or warehousing, and without the copyright owner making a profit.</p>
           <p></p>
