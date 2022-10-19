56c7b499acabf2acb3adedac7ce565867c8e0c2d
Chaitanya1603 <56607472+chaitanya99999999@users.noreply.github.com>
2022-10-19 05:09:24 -0400

    added about page along with the jas libraries

---
+++ about.php
@@ -0,0 +1,36 @@
+<?php include_once("includes/header.php"); ?>
+
+
+    <div style="background-color:white; color:black" class="breadcrumbs">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-sm-4">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row mar-b-50">
+        <div class="col-lg-5">
+          <div class="about-carousel wow fadeInLeft">
+            <div id="myCarousel" class="carousel slide">
+              <div class="carousel-inner">
+                <div class="active item">
+                  <img src="img/hmd2.jpeg" alt="">
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div style="margin-bottom:130px"class="col-lg-7 about wow fadeInRight">
+          <h3>About HandMeDown</h3>
+          <p>In this platform users willing to sell their books can set their own price and state the book condition, if any customer intends to buy the book he/she may contact the seller through messaging provided on the website itself.As the books are sold by students no maintanence cost is charged like in stores and you can get books for cheap proffiting both the seller and buyer , thus making online purchasing cheaper than the offline one. We would provide general registration and login functionality for users. They can browse through the website , search for their required books and consequently add to cart if they do not wish to purchase immediately otherwise directly order/purchase them. Similarly users who intend to sell their books can upload the details of the books and post online for potential customers to view and purchase.</p>
+          <p>Online Book Exchange is the practice of a swap of books between one person and another. Practiced among book groups, friends and colleagues at work, it provides an inexpensive way for people to exchange books, find out about new books and obtain a new book to read without having to pay. Because swaps occur between individuals, without central distribution or warehousing, and without the copyright owner making a profit.</p>
+          <p></p>
+          </div>
+      </div>
+
+    </div>
+<?php include_once("includes/footer.php"); ?>
